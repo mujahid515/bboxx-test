cd84a589db3c60f9f0e7c806d3d69016cb807f17
Mujahid <mujahid.chowdhury515@gmail.com>
2019-09-26 10:42:41 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JwPaginationComponent } from 'jw-angular-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JwPaginationComponent
   ],
   imports: [
     BrowserModule,
