d85307695eb12c27f323c1d1aab0a17af027380c
Mujahid <mujahid.chowdhury515@gmail.com>
2019-09-26 10:44:45 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bboxx-app';
+  items = [];
+  pageOfItems: Array<any>;
+
+  constructor() { }
+
+  ngOnInit() {
+      //Get JSON from API and push to items
+      
+
+      // an example array of 150 items to be paged
+      this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1), name: `Item ${i + 1}`}));
+  }
+
+  onChangePage(pageOfItems: Array<any>) {
+      // update current page of items
+      this.pageOfItems = pageOfItems;
+  }
 }
